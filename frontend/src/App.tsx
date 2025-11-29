6b0176dc1578c804e245ae8cc339030cd9fca422
Dawid Kozaczuk <dk312091@student.polsl.pl>
2025-11-29 13:02:03 +0100

    feat: restructure App component layout and integrate daisyui plugin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,12 @@ function App() {
   return (
     <>
     <div className='flex w-full flex-col items-center justify-center h-screen bg-gray-900'>
-      <p className='text-amber-300'>Hacakgton</p>
+      <div className="card bg-base-100">
+      <div className="card-body">
+
+        <p className='text-amber-300'>Hacakgton</p>
+      </div>
+      </div>
     </div>
     </>
   )
