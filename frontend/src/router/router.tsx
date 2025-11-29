32401a3a0563723026fcb8ed567b6c943049eb57
Kacper Stępień <kacpersosnicaga@gmail.com>
2025-11-29 15:21:14 +0100

    v1 fix

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "@/pages/auth/login/LoginPage";
 import App from "@/App";
 
-import { action as loginAction } from "@/pages/auth/login/action";
-
 export const router = createBrowserRouter([
   {
     id: "root",
@@ -13,6 +11,5 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginPage />,
-    action: loginAction,
   },
 ]);
