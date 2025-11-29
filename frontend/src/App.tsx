f6394d1e04dbf7e4dd963e014ecfb749179f44fd
Dawid Kozaczuk <dk312091@student.polsl.pl>
2025-11-29 12:50:21 +0100

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Included Vite logo SVG in public directory
    - Created main application component (App.tsx) with basic layout
    - Set up global CSS file (index.css) with Tailwind CSS import
    - Configured main entry point (main.tsx) for React application
    - Established TypeScript configuration files for app and node
    - Configured Vite with React and Tailwind CSS plugins

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex w-full flex-col items-center justify-center h-screen bg-gray-900'>
+      <p className='text-amber-300'>Hacakgton</p>
+    </div>
+    </>
+  )
+}
+
+export default App
