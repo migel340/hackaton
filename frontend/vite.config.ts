eaaf76ed5101d2cd5ae96d2e6bb237247ed1717a
Dawid Kozaczuk <dk312091@student.polsl.pl>
2025-11-29 12:58:54 +0100

    feat: set up routing with react-router and configure path aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@layouts': '/src/layouts',
+      '@components': '/src/components',
+      '@api': '/src/api',
+      '@router': '/src/router',
+      '@utils': '/src/utils',
+      '@hooks': '/src/hooks',
+      '@styles': '/src/styles',
+      '@pages': '/src/pages',
+    },
+  }
 })
