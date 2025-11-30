819fb3628066f4117f2db41a8575ab96d6c25d22
Kacper Stępień <kacpersosnicaga@gmail.com>
2025-11-30 06:26:21 +0100

    Finish profile page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': '/src',
-      '@layouts': '/src/layouts',
-      '@components': '/src/components',
-      '@api': '/src/api',
-      '@router': '/src/router',
-      '@utils': '/src/utils',
-      '@hooks': '/src/hooks',
-      '@styles': '/src/styles',
-      '@pages': '/src/pages',
+      "@": "/src",
+      "@layouts": "/src/layouts",
+      "@components": "/src/components",
+      "@api": "/src/api",
+      "@router": "/src/router",
+      "@utils": "/src/utils",
+      "@hooks": "/src/hooks",
+      "@styles": "/src/styles",
+      "@pages": "/src/pages",
     },
   },
-   server: {
+  server: {
     proxy: {
-      '/local': {
-        target: 'http://localhost:4000',
+      "/local": {
+        target: "http://localhost:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/local/, ''),
+        rewrite: (path) => path.replace(/^\/local/, ""),
       },
     },
   },
-})
+});
